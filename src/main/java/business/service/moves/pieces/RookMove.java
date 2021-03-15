a543e88b6706100a4df55a8887346839b459541c
josehinojosaacevedo <66885102+josehinojosaacevedo@users.noreply.github.com>
2021-03-14 20:03:19 -0500

    patron Factory en calculateMoves

--- src/main/java/business/service/moves/pieces/RookMove.java
+++ src/main/java/business/service/moves/pieces/RookMove.java
@@ -1,22 +1,20 @@
 package business.service.moves.pieces;
 
 import business.service.moves.cardinal.CalculateCardinalMove;
-import business.service.moves.cardinal.impl.CalculateEastMoves;
-import business.service.moves.cardinal.impl.CalculateNorthMoves;
-import business.service.moves.cardinal.impl.CalculateSouthMoves;
-import business.service.moves.cardinal.impl.CalculateWestMoves;
+import business.service.moves.cardinal.CalculateMovesFactory;
+import util.CardinalPoint;
 import util.ColorOfPiece;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class RookMove implements ICreateMove{
 
     @Override
     public List<CalculateCardinalMove> createMove(int pieceRow, int pieceColumn, ColorOfPiece color){
-        return Arrays.asList(new CalculateSouthMoves(pieceRow,pieceColumn,color),
-                new CalculateNorthMoves(pieceRow,pieceColumn,color),
-                new CalculateEastMoves(pieceRow,pieceColumn,color),
-                new CalculateWestMoves(pieceRow,pieceColumn,color));
+        return CalculateMovesFactory.getInstance(pieceRow, pieceColumn, color,
+            CardinalPoint.SOUTH,
+            CardinalPoint.NORTH,
+            CardinalPoint.EAST,
+            CardinalPoint.WEST);
     }
 }
