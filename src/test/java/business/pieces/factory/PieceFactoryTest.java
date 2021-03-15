4c46a5470ff752126458262aaf8f0c809b1faadd
Osti Katriel <katriel181920@example.com>
2021-03-15 00:29:54 -0500

    test factory

---
+++ src/test/java/business/pieces/factory/PieceFactoryTest.java
@@ -0,0 +1,19 @@
+package business.pieces.factory;
+
+import business.pieces.*;
+import gui.board.ChessGameBoard;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PieceFactoryTest {
+
+
+
+
+}
