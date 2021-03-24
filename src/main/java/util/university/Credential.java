d96dbbcab173c7e20a1f77b21db164b080ec87a7
Osti Katriel <katriel181920@example.com>
2021-03-24 13:19:49 -0500

    create credentials

---
+++ src/main/java/util/university/Credential.java
@@ -0,0 +1,17 @@
+package util.university;
+
+import sun.util.PreHashedMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Credential {
+
+    public static final Map<String,String> CODE=new HashMap<String,String>(){{
+        put("admin", "admin");
+        put("17200320", "Osti Katriel Barrozo Figueroa");
+        put("17200321", "value2");
+        put("17200322", "value2");
+
+    }};;
+}
