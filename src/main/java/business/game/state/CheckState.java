5a286a37a5b484c5d878a803b72279aaa0bc01e3
josehinojosaacevedo <66885102+josehinojosaacevedo@users.noreply.github.com>
2021-04-09 22:51:13 -0500

    Funcionalidad color de celda del Rey cuando está en jaque

--- src/main/java/business/game/state/CheckState.java
+++ src/main/java/business/game/state/CheckState.java
@@ -1,24 +1,41 @@
 package business.game.state;
 
+import gui.board.ChessGameBoard;
+
+import java.awt.Color;
+
 import business.game.ChessGameEngine;
+import business.pieces.King;
 
 public class CheckState extends AbstractState {
 
-    int playerInCheck;
+    private int playerInCheck;
 
-    public CheckState(ChessGameEngine gameEngine, int playerInCheck) {
-		super(gameEngine);
+    public CheckState(ChessGameEngine gameEngine,ChessGameBoard board, King king1,King king2,int playerInCheck) {
+		super(gameEngine,board,king1,king2);
 		this.playerInCheck = playerInCheck;
 	}
 
 	@Override
-	public int getStateName() {
+	public int getType() {
 		return State.CHECK;
 	}
 
 	@Override
-	public int getPlayer() {
+	public int getPlayerInCheck() {
 		return playerInCheck;
 	}
 
+	@Override
+	public void update() {
+		King king = getKing(playerInCheck);
+		this.board.getCell(king.getRow(),king.getColumn()).setBackground(Color.RED);
+	}
+
+	@Override
+	public void beforeChange(State state){
+		if(state.getType() == State.NORMAL){
+			this.board.clearColorsOnBoard();
+		}
+	}
 }
