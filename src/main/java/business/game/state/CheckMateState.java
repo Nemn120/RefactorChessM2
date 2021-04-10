aacdbbb14bcfd7c867bf7b4b98b8bc92accf9b7c
josehinojosaacevedo <66885102+josehinojosaacevedo@users.noreply.github.com>
2021-04-09 22:48:43 -0500

    Clases de los Estados actualizadas

--- src/main/java/business/game/state/CheckMateState.java
+++ src/main/java/business/game/state/CheckMateState.java
@@ -1,24 +1,43 @@
 package business.game.state;
 
 import business.game.ChessGameEngine;
+import business.pieces.King;
+import gui.board.ChessGameBoard;
 
 public class CheckMateState extends AbstractState {
 
-	int lostPlayer;
+	private int loser;
     
-    public CheckMateState(ChessGameEngine gameEngine,int lostPlayer) {
-		super(gameEngine);
-		this.lostPlayer = lostPlayer;
+    public CheckMateState(ChessGameEngine gameEngine,ChessGameBoard board,King king1,King king2,int loser) {
+		super(gameEngine,board,king1,king2);
+		this.loser = loser;
 	}
 
 	@Override
-	public int getStateName() {
+	public int getType() {
 		return State.CHECKMATE;
 	}
 
 	@Override
-	public int getPlayer() {
-		return lostPlayer;
+	public int getWinner() {
+		return loser == 1 ? 2 : 1;
+	}
+
+	@Override
+	public int getLoser() {
+		return loser;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeChange(State state) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/main/java/business/game/state/NormalState.java
+++ src/main/java/business/game/state/NormalState.java
@@ -1,16 +1,30 @@
 package business.game.state;
 
 import business.game.ChessGameEngine;
+import business.pieces.King;
+import gui.board.ChessGameBoard;
 
 public class NormalState extends AbstractState {
     
-    public NormalState(ChessGameEngine gameEngine) {
-		super(gameEngine);
+    public NormalState(ChessGameEngine gameEngine,ChessGameBoard board,King king1,King king2) {
+		super(gameEngine,board,king1,king2);
 	}
 
 	@Override
-	public int getStateName() {
+	public int getType() {
 		return State.NORMAL;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeChange(State state) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
