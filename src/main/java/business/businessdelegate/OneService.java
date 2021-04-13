fe73e98bfe3d788ff6dc983df3ca95f76cc269fd
Osti Katriel <katriel181920@example.com>
2021-04-13 16:55:04 -0500

    AudioClip

--- src/main/java/business/businessdelegate/OneService.java
+++ src/main/java/business/businessdelegate/OneService.java
@@ -1,8 +1,12 @@
 package business.businessdelegate;
 
+import java.applet.AudioClip;
+
 public class OneService implements BusinessService{
     public void doProcessing()
     {
         System.out.println("Processed Service One");
+        AudioClip audio=java.applet.Applet.newAudioClip(getClass().getResource("/main/java/musics/BellaCiao.mp3"));
+        audio.play();
     }
 }
